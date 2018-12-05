5e3004ccf3ad587652b9528a839f861d67f107b0
kino <lihongxuan213@gmail.com>
2018-12-05 19:30:59 +0800

    implement sandbox

--- pcpServer.go
+++ pcpServer.go
@@ -1,5 +1,10 @@
 package gopcp
 
+import (
+	"encoding/json"
+	"reflect"
+)
+
 // function node
 type FunNode struct {
 	funName string
@@ -9,13 +14,45 @@ type FunNode struct {
 // simpe calling protocol
 // grammer based on json
 // ["fun1", 1, 2, ["fun2", 3]] => fun1(1, 2, fun2(3))
-
 type PcpServer struct {
 	sandbox Sandbox
 }
 
-func (pcpServer PcpServer) parseJson(source string) {}
+func (pcpServer PcpServer) parseJSON(source string) (*FunNode, error) {
+	arr := []interface{}{}
+	if err := json.Unmarshal([]byte{source}, &arr); err != nil {
+		return err
+	}
+	return parseAst(arr)
+}
 
-//func getPcpServer(sandbox Sandbox) PcpServer {
-// merge sandbox with default sandbox
-//}
+func parseAst(arr []interface{}) (node *FunNode, err error) {
+	if len(arr) == 0 {
+		return
+	}
+	funName := arr[0]
+	if reflect.TypeOf(funcName).Kind() != reflect.String {
+		err = error.New("first element in array must be string")
+		return
+	}
+	curNode := &FunNode{funName: funName, params: []interface{}{}}
+	for i, v := range arr[1:] {
+		val := reflect.ValueOf(v)
+		if val.Kind() == reflect.Array {
+			var newArr []interface{}
+			copy(newArr[:], val)
+			newNode, err := parseAst(newArr)
+			curNode.params = append(curNode.params, newNode)
+		} else {
+			curNode.params = append(curNode.params, val)
+		}
+	}
+	return
+}
+
+// NewPcpServer merge sandbox with default sandbox
+func NewPcpServer(sandbox *Sandbox) PcpServer {
+	box := NewSandbox(defBox)
+	defSandBox.Extend(sandbox)
+	return PcpServer{box}
+}
--- sandbox.go
+++ sandbox.go
@@ -1,12 +1,55 @@
 package gopcp
 
+import "fmt"
+
 type GeneralFun = func([]interface{}, PcpServer) interface{}
 
-// funType: 0 -> normal sandbox function
-// funType: 1 -> lazy sandbox function
+// SandBoxType
+const (
+	SandboxTypeNormal = "normal_sandbox_type"
+	SandboxTypeLazy   = "lazy_sandbox_type"
+)
+
+// BoxFun
 type BoxFun struct {
-	funType int
+	funType string // SandBoxType
 	fun     GeneralFun
 }
 
-type Sandbox struct{}
+// Sandbox
+type Sandbox struct {
+	funcMap map[string]BoxFunc
+}
+
+func NewSandbox(val *map[string]BoxFunc) {
+	sandbox := &SandBox{}
+	if val != nil {
+		sandbox.funcMap = *val
+	} else {
+		sandbox.funcMap = map[string]BoxFunc{}
+	}
+	return sandbox
+}
+
+// Get get sandbox method
+func (sandBox *SandBox) Get(name string) (*BoxFunc, error) {
+	if val, ok := sandBox.funcMap[name]; ok {
+		return &val, nil
+	}
+	return nil, error.New(fmt.Sprintf("function [%s] doesn't exist in sandBox", name))
+}
+
+// Set set sandbox method
+func (sandBox *SandBox) Set(name string, val BoxFunc) {
+	sandBox.funcMap[name] = val
+}
+
+// Extend merge newSandBox's value to origin sandBox
+func (sandBox *SandBox) Extend(newSandBox *SandBox) {
+	if newSandBox == nil {
+		return
+	}
+	for k, v := range newSandBox.funcMap {
+		sandBox.Set(k, v)
+	}
+}
