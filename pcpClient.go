69597c707aafa1815951f8da7f72442a15e79ff2
kino <lihongxuan213@gmail.com>
2018-12-07 19:29:02 +0800

    pcp server

--- pcpClient.go
+++ pcpClient.go
@@ -1,1 +1,14 @@
 package gopcp
+
+import "encoding/json"
+
+//PcpClient pcp client
+type PcpClient struct{}
+
+//Call call function in  pcp server
+func (c *PcpClient) Call(funName string, params []interface{}) (interface{}, error) {
+}
+
+func (c *PcpClient) ToJSON(res interface{}) (string, error) {
+	return json.Marshal(res)
+}
--- pcpServer.go
+++ pcpServer.go
@@ -5,25 +5,60 @@ import (
 	"reflect"
 )
 
-// function node
+// FunNode function node
 type FunNode struct {
 	funName string
 	params  []interface{}
 }
 
-// simpe calling protocol
+// PcpServer simpe calling protocol
 // grammer based on json
 // ["fun1", 1, 2, ["fun2", 3]] => fun1(1, 2, fun2(3))
 type PcpServer struct {
 	sandbox Sandbox
 }
 
-func (pcpServer PcpServer) parseJSON(source string) (*FunNode, error) {
+func parseJSON(source string) (*FunNode, error) {
 	arr := []interface{}{}
 	if err := json.Unmarshal([]byte{source}, &arr); err != nil {
 		return err
 	}
 	return parseAst(arr)
+}
+
+// Execute ....
+func (pcpServer *PcpServer) Execute(source string) (interface{}, error) {
+	node, err := parseJSON(source)
+	if err != nil {
+		return err
+	}
+	return pcpServer.executeAst(node)
+}
+
+func (pcpServer *PcpServer) executeAst(node *FunNode) (interface{}, error) {
+	if node != nil {
+		fn := pcpServer.sandbox.get(node.funName)
+		funcType := fn.funType
+		fun := fn.fun
+		params := []interface{}{}
+		if funcType == SandboxTypeNormal {
+			for field := range node.params {
+				var res interface{}
+				switch field.(type) {
+				case FunNode:
+					funcNode := field.(FunNode)
+					res = executeAst(funcNode)
+				default:
+					res = field
+				}
+				params = append(params, res)
+			}
+			res, err := fun(params...)
+			return res, err
+		} else if funcType == SandboxTypeLazy {
+			return nil, nil
+		}
+	}
 }
 
 func parseAst(arr []interface{}) (node *FunNode, err error) {
@@ -51,8 +86,8 @@ func parseAst(arr []interface{}) (node *FunNode, err error) {
 }
 
 // NewPcpServer merge sandbox with default sandbox
-func NewPcpServer(sandbox *Sandbox) PcpServer {
+func NewPcpServer(sandbox *Sandbox) *PcpServer {
 	box := NewSandbox(defBox)
 	defSandBox.Extend(sandbox)
-	return PcpServer{box}
+	return &PcpServer{box}
 }
